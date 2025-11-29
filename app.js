bc71a407e56646ee7f460a633a6e091a6a4e5747
umarfarooque09 <muhammadumarfarooque569@gmail.com>
2025-11-29 10:36:35 +0500

    JavaScript1

---
+++ app.js
@@ -0,0 +1,65 @@
+    // Task Number: 1
+
+// var arr = [1, 2, 5, 3, 4, 5, 6, 7, 8, 8, 10];
+// var count = 0;
+
+// for (var i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 == 0){  
+//         count++;
+//     }
+// }
+
+// document.writeln("Array: [ " + arr + " ]");
+// document.writeln("<br/>Total even numbers = " + count);
+
+
+    // Task Number: 2
+
+// var arr = [1, 2, 5, 3, 4, 5, 6, 7, 8, 8, 10];
+// var unique = [];
+
+// for (var i = 0; i < arr.length; i++) {
+//     var found = false;
+
+//     for (var j = 0; j < unique.length; j++) {
+//         if (arr[i] === unique[j]) {
+//             found = true; 
+//         }
+//     }
+
+//     if (found === false) {
+//         unique.push(arr[i]); 
+//     }
+// }
+
+// document.writeln("Given Array: [ "+ arr + " ]");
+// document.writeln("<br/>Unique Array: [ "+ unique + " ]");
+
+
+    // Task Number: 3
+
+//     var ratings = [5, 4, 5, 3, 5, 4, 2];
+// var count = 0;
+
+// for (var i = 0; i < ratings.length; i++) {
+//     if (ratings[i] == 5) {
+//         count++;
+//     }
+// }
+
+// document.writeln("All Ratings: [ "+ ratings + " ]");
+// document.writeln("<br/>5-star ratings = " + count);
+
+
+    // Task Number: 4
+
+//     var temps = [98.4, 101.1, 99.0, 102.5, 100.1];
+
+// for (var i = 0; i < temps.length; i++) {
+//     if (temps[i] > 100.4) {
+//         document.writeln(temps[i] + " → Fever <br>");
+//     } 
+//     else {
+//         document.writeln(temps[i] + " → Normal <br>");
+//     }
+// }
